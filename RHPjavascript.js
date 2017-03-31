497cf772ee1eb5bde748b9a24ce8e56c3e04b516
austinbowler <austingrantbowler@gmail.com>
2017-03-31 09:53:53 -0600

    I don’t even know anymore

--- RHPjavascript.js
+++ RHPjavascript.js
@@ -1,13 +1,55 @@
-$(document).ready(function () {
-	var jsonURL = "testimagejson.json";
-	$.getJSON(jsonURL, function (json)
-			  {
-		var imgList= "";
+	function myFunction() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Whats%20Your%20Favorite%20Superhero&body=I%20love%20Spiderman%20because%20he%20is%20the%20best%20and%20so%20am%20I!');
+}
 
-		$.each(json.products, function () {
-			imgList += '<p><img src= "' + this.imgPath + '"></p>';
-		});
+function myFunction2() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Whats%20Your%20Favorite%20Superhero&body=I%20love%20Batman%20because%20he%20is%20dark%20like%20my%20soul!');
+}
 
-		$('#dvProdList').append(imgList);
-	});
-});
+function myFunction3() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Whats%20Your%20Favorite%20Superhero&body=I%20love%20Superman%20because%20he%20is%20obviously%20the%20most%20powerful!');
+}
+
+function myFunction4() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Whats%20Your%20Favorite%20Superhero&body=I%20love%20Wonder%20Woman%20because%20she%20is%20awesome!');
+}
+
+function myFunction5() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Whats%20Your%20Favorite%20Superhero&body=I%20love%20Captian%20America%20because%20AMERICA!!!');
+}
+
+function geters() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Who%20do%20you%20want%20to%20see&body=Get%20Batman%20before%20Batman%20gets%20you!!!');
+}
+
+function geters2() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Who%20do%20you%20want%20to%20see&body=Get%20Wonder%20Woman...%20as%20long%20as%20her%20skirt%20is%20longer!!!');
+}
+
+function geters3() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Who%20do%20you%20want%20to%20see&body=Get%20Superman%20because%20he%20is%20the%20best!!!');
+}
+
+function geters4() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Who%20do%20you%20want%20to%20see&body=WHAT!?!?%20I%20cant%20believe%20you%20forgot%20__________!!!%20Add%20him/her%20to%20the%20list!!!');
+}
+
+function salad() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Where%20is%20Waldo?&body=I%20have%20no%20idea.');
+}
+
+function salad2() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Where%20is%20Waldo?&body=Wherever%20Carmen%20Sandiego%20is.');
+}
+
+function salad3() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Where%20is%20Waldo?&body=At%20my%20house.');
+}
+
+function salad4() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Where%20is%20Waldo?&body=In%20the%20corner%20of%20the%20page.');
+}
+
+function salad5() {
+	window.open('mailto:rexburgheroparty@gmail.com?Subject=Where%20is%20Waldo?&body=I%20found%20him%20on%20Google%20Earth.');
+}
